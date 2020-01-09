0614d2753b43173d7cd2e7db3f86d3c0718bbaeb
JaylenJames <jlnjms94@gmail.com>
2020-01-09 16:19:08 -0600

    reaction force script included in folder

---
+++ reaction_force_calculation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  4 13:30:13 2020
+
+Calculation of ground reaction force on the foot.
+
+@author: JaylenJames
+"""
+import pandas as pd
+
+squat_data = pd.read_csv()
+#Import CSV as a datafrome
+
+#Remove rows that aren;t neeted
+
+
+#Determine number of cells that are non-zero
+
+#Sum these values and also calculate their surface area.
+#   Row Spacing = 0.33 in
+#   Col Spaceing = 0.33 in
+
+
+
+
